d55e5d42aadc312823c9dcf66a1474d8b0c3431c
Lucas Connors <lucas.revolutiontech@gmail.com>
2016-12-04 00:26:28 -0800

    Remove all other/another nonsense

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,22 +4,7 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   template: `
     <h1>Inline Template</h1>
-    <fa-other></fa-other>
-    <fa-another>
-      <div>
-        <h1>Hello!</h1>
-        <p>World!</p>
-      </div>
-    </fa-another>
-    <fa-another>
-      <p>Something else</p>
-    </fa-another>
-  `,
-  styles: [`
-    h1 {
-      color: red;
-    }
-  `]
+  `
 })
 export class AppComponent {
   title = 'I changed it!';
